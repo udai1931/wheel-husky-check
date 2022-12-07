4b0e236c929c83985681f13d65b287ffc906c0dc
Udai Gupta <udaigupta19311@gmail.com>
2022-12-07 14:28:09 +0530

    qasxc

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,7 +9,6 @@ class ApplicationController < ActionController::Base
     def ensure_current_user_is_superadmin!
       # qwerty
       authenticate_user!
-======== 
       unless current_user.super_admin?
         redirect_to root_path, status: :forbidden, alert: "Unauthorized Access!"
       end
